76e53860c3df49d5135bceb1cd8fe3437d9bbe40
Alexy845 <alexy.daubresse@ynov.com>
2024-01-15 16:15:20 +0100

    death

--- Assets/Script/FlyFlappy.cs
+++ Assets/Script/FlyFlappy.cs
@@ -2,23 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class FlyFlappy : MonoBehaviour
 {
-
-    // Start is called before the first frame update
     private float speed = 1.5f;
     private float rotationSpeed = 10f;
 
     private Rigidbody2D rb2D;
+    private bool isDead = false;
+
     void Start()
     {
-     rb2D = GetComponent<Rigidbody2D>();   
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Vérifiez si le joueur est mort, si oui, ignorez les entrées
+        if (isDead)
+        {
+            return;
+        }
+
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             rb2D.velocity = Vector2.up * speed;
@@ -27,6 +33,33 @@ public class FlyFlappy : MonoBehaviour
 
     private void FixedUpdate()
     {
-        transform.rotation = Quaternion.Euler(0,0, rb2D.velocity.y * rotationSpeed);
+        // Vérifiez si le joueur est mort, si oui, ignorez les rotations
+        if (!isDead)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, rb2D.velocity.y * rotationSpeed);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Death"))
+        {
+            // Marquez le joueur comme mort
+            isDead = true;
+
+            // Vous pouvez également désactiver d'autres composants ou effectuer d'autres actions ici si nécessaire
+
+            // Retardez le chargement de la scène pour laisser le temps à l'animation de mort de jouer
+            StartCoroutine(LoadNextScene());
+        }
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        // Attendez quelques secondes avant de charger la scène suivante
+        yield return new WaitForSeconds(2f);
+
+        // Chargez la scène suivante
+        SceneManager.LoadScene(1);
     }
 }
